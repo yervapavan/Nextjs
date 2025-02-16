8f5cf709f9143fa379c1134e60b2a0e70135ff16
yerva pavan <yervapavan16@gmail.com>
2025-02-16 23:58:38 +0530

    custom Not found page

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,7 +1,11 @@
+import { notFound } from "next/navigation"
 export default function docs({params}:{params:{
         slug:string[]
 }}){
                 if(params.slug?.length==2){
+                        if (params.slug[1]=="1001"){
+                                return notFound()
+                        }
                         return(
                         <h1>This is the doc page of {params.slug[0]} of the concept {params.slug[1]} </h1>)
                 }
