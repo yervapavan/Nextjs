2e9c75b666adafdd913c25f9fffdc11ab482fd89
yerva pavan <yervapavan16@gmail.com>
2025-02-24 22:31:59 +0530

    activ liks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
+"use client"
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export const metadata: Metadata = {
+ const metadata: Metadata = {
   title: {
     default: "Next.js",
     template: "%s | Next.js",
@@ -9,14 +12,29 @@ export const metadata: Metadata = {
   description: "Root page",
 };
 
+const links=[
+  {name:"Register",href:"/register"},
+  {name:"Login",href:"/login"},
+]
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname=usePathname()
   return (
     <html lang="en">
       <body>
+      {links.map((link)=>{
+        const isActive= pathname===link.href || pathname.startsWith(`${link.href}/`) && link.href!="/"
+        return(
+          <>
+          <Link className={isActive? "font-bold mr-4":"text-blue-600"} href={link.href}>{link.name}</Link>
+          <br/>
+          </>
+        )
+      })}
        <h1 className="w-screen h-14 text-yellow-400 bg-transparent flex tex-center items-center justify-center">Header</h1>
         {children}
         <h1 className="w-screen h-14 text-yellow-400 bg-transparent flex tex-center items-center justify-center">Footer</h1>
