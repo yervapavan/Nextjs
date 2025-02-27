4eab2fda9886becc7132a96fb241af934a57ff33
yerva pavan <yervapavan16@gmail.com>
2025-02-27 23:26:33 +0530

    loading.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,12 @@
-export default function blog(){
+export default async function blog(){
+        await new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                        resolve("resolved")
+                },2000)
+        })
         return(
                 <>
                 <h1>Blog</h1>
                 </>
         )
-}
+}       
