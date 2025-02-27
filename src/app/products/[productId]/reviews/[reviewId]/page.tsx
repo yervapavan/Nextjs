0f3563e2c20ba57143c13cf207fcda8896a57e92
yerva pavan <yervapavan16@gmail.com>
2025-02-27 23:11:44 +0530

    params

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,12 @@
-export default function reviews({params}:{params:{productId:string,reviewId:string}}){
+"use client"
+import { use } from "react"
+export default function reviews({params}:
+        {params:Promise<{productId:string,reviewId:string}>}){
+        const {productId,reviewId} = use(params)
         return(
-                <h1>this is Review of {params.productId}</h1>
+                <>
+                <h1>this is Review of {productId}</h1>
+                <h2>reviewId: {reviewId}</h2>
+                </>
         )
 }
